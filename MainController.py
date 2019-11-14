e3b2b30b3fbb6d7f4fa6470e19cd6474de4f3325
Leonardo Coelho <leonardo.coelho@hitachivantara.com>
2019-11-14 16:20:57 +0000

    Detect Merge Conflits

--- MainController.py
+++ MainController.py
@@ -110,13 +110,16 @@ class MainController:
             repositories = raw_data['detail'][0]['repositories']
 
             base_pr = version_pr = None
+            has_merge_conflicts = False
             jira_comment = "PRs:"
             base_bug = JIRAUtils.get_base_bug(self.jira_connection, sp_key)
             jira_comments = [[re.search(r'(?<=PR:).*', body).group(0) for body in
-                              comment.body.encode("ascii", errors="ignore").decode().replace("\r\n", "\n").replace("\r", "\n").split('\n') if
+                              comment.body.encode("ascii", errors="ignore").decode().replace("\r\n", "\n").replace("\r",
+                                                                                                                   "\n").split(
+                                  '\n') if
                               re.search(r'(?<=PR:).*', body) is not None] for comment in
                              self.jira_connection.issue(base_bug.key).fields.comment.comments]
-            links_in_comments = [item for sublist in jira_comments for item in sublist]
+            links_in_comments = [item.strip() for sublist in jira_comments for item in sublist]
             rep_names = [rep['name'] for rep in repositories]
 
             for rep_name in rep_names:
@@ -130,7 +133,10 @@ class MainController:
                         upstream_repo = upstream_user.get_repo(rep_name)
                     except GithubException:
                         upstream_repo = self.github_connection.get_user('webdetails').get_repo(rep_name)
-                    pr = upstream_repo.get_pull(not_missing_link.split('/')[-1])
+                    try:
+                        pr = upstream_repo.get_pull(not_missing_link.split('/')[-1])
+                    except:
+                        continue
                     for commit in pr.get_commits().get_page(0):
                         rep_name = commit.html_url.split('/')[4]
                         sha = commit.html_url.split('/')[-1]
@@ -148,7 +154,10 @@ class MainController:
                         upstream_repo = upstream_user.get_repo(rep_name)
                     except GithubException:
                         upstream_repo = self.github_connection.get_user('webdetails').get_repo(rep_name)
-                    pr = upstream_repo.get_pull(pr_nr)
+                    try:
+                        pr = upstream_repo.get_pull(pr_nr)
+                    except:
+                        continue
                     for commit in pr.get_commits().get_page(0):
                         rep_name = commit.html_url.split('/')[4]
                         sha = commit.html_url.split('/')[-1]
@@ -158,6 +167,7 @@ class MainController:
                                                   'authorTimestamp': 1}]})
 
             for repository in repositories:
+                has_merge_conflicts = False
                 self.gui.log_info("Creating the " + sp_key + " branch in " + repository['name'] + ".")
 
                 # Verify repository path.
@@ -174,15 +184,19 @@ class MainController:
                 sp_version_branch = self.service_pack.split(' ')[1].replace('(', '').replace(')', '')
                 git.fetch('--all')
                 try:
+                    git.push("origin", '--delete', sp_key)
+                except:
+                    pass
+                try:
                     git.checkout('-b', base_version_branch, 'origin/' + base_version_branch)
                 except g.GitCommandError as gce:
                     git.checkout(base_version_branch)
                 git.pull('upstream', base_version_branch)
                 try:
-                    git.checkout('-b', sp_key)
-                except g.GitCommandError as gce:
-                    self.gui.log_error("Failed to create branch: " + gce.stderr.strip())
-                    continue
+                    git.branch("-D", sp_key)
+                except:
+                    pass
+                git.checkout('-b', sp_key)
 
                 # Cherry-Pick commits.
                 commits = repository['commits']
@@ -202,18 +216,23 @@ class MainController:
                         try:
                             git.cherry_pick(sha)
                         except g.GitCommandError as gce:
+                            has_merge_conflicts = True
                             self.gui.log_error("Unable to cherry-pick '" + sha + "': " + gce.stderr.strip())
+                            git.cherry_pick('--abort')
                             continue
                         # Rename commits with backport message.
                         git.commit('--amend', '-m', commit_message)
 
                 # Push commits to origin's SP branch.
-                try:
-                    self.gui.log_info("Pushing commits to " + sp_key + " branch.")
-                    git.push("origin", sp_key)
-                except g.GitCommandError as gce:
-                    self.gui.log_error("Unable to push changes to origin " + sp_key + " branch: " + gce.stderr.strip())
-                    continue
+                if has_merge_conflicts is False:
+                    try:
+                        self.gui.log_info("Pushing commits to " + sp_key + " branch.")
+                        git.push("origin", sp_key)
+                    except g.GitCommandError as gce:
+                        self.gui.log_error("Unable to push changes to origin " + sp_key + " branch: " + gce.stderr.strip())
+                        git.checkout('master')
+                        git.branch("-D", sp_key)
+                        self.gui.log_info("Done with " + repository['name'] + "!")
 
                 # Build PR message.
                 self.master1 = self.gui.master1_input.get()
@@ -231,40 +250,41 @@ class MainController:
                     upstream_repo = self.github_connection.get_user('webdetails').get_repo(repository['name'])
 
                 # For base version branch
-                try:
-                    upstream_repo.get_branch(base_version_branch)
-                    base_pr = upstream_repo.create_pull(commit_message, pr_message, base_version_branch,
-                                                        '{}:{}'.format(self.github_username, sp_key), True)
-                    base_pr
-                except GithubException as ge:
-                    if ge.status == 422:
-                        self.gui.log_error(
-                            "Unable to submit PR for " + sp_key + " in " + base_version_branch + " branch: " +
-                            ge.data['errors'][0]['message'])
+                if has_merge_conflicts is False:
+                    try:
+                        upstream_repo.get_branch(base_version_branch)
+                        base_pr = upstream_repo.create_pull(commit_message, pr_message, base_version_branch,
+                                                            '{}:{}'.format(self.github_username, sp_key), True)
+                        base_pr
+                    except GithubException as ge:
+                        if ge.status == 422:
+                            self.gui.log_error(
+                                "Unable to submit PR for " + sp_key + " in " + base_version_branch + " branch: " +
+                                ge.data['errors'][0]['message'])
+                        else:
+                            self.gui.log_error(
+                                "Unable to submit PR for " + sp_key + " in " + base_version_branch + " branch: " +
+                                ge.data['message'])
                     else:
-                        self.gui.log_error(
-                            "Unable to submit PR for " + sp_key + " in " + base_version_branch + " branch: " +
-                            ge.data['message'])
-                else:
-                    self.gui.log_info("Opened Pull Request in " + base_version_branch + " branch")
+                        self.gui.log_info("Opened Pull Request in " + base_version_branch + " branch")
 
-                # For SP branch
-                try:
-                    upstream_repo.get_branch(sp_version_branch)
-                    version_pr = upstream_repo.create_pull(commit_message, pr_message, sp_version_branch,
-                                                           '{}:{}'.format(self.github_username, sp_key), True)
-                    version_pr
-                except GithubException as ge:
-                    if ge.status == 422:
-                        self.gui.log_error(
-                            "Unable to submit PR for " + sp_key + " in " + sp_version_branch + " branch: " +
-                            ge.data['errors'][0]['message'])
+                    # For SP branch
+                    try:
+                        upstream_repo.get_branch(sp_version_branch)
+                        version_pr = upstream_repo.create_pull(commit_message, pr_message, sp_version_branch,
+                                                               '{}:{}'.format(self.github_username, sp_key), True)
+                        version_pr
+                    except GithubException as ge:
+                        if ge.status == 422:
+                            self.gui.log_error(
+                                "Unable to submit PR for " + sp_key + " in " + sp_version_branch + " branch: " +
+                                ge.data['errors'][0]['message'])
+                        else:
+                            self.gui.log_warn(
+                                "Unable to submit PR for " + sp_key + " in " + sp_version_branch + " branch: " +
+                                ge.data['message'])
                     else:
-                        self.gui.log_warn(
-                            "Unable to submit PR for " + sp_key + " in " + sp_version_branch + " branch: " +
-                            ge.data['message'])
-                else:
-                    self.gui.log_info("Opened Pull Request in " + sp_version_branch + " branch")
+                        self.gui.log_info("Opened Pull Request in " + sp_version_branch + " branch")
 
                 # Delete branch and Move to next repository.
                 self.gui.log_info("Deleting " + sp_key + " branch...")
@@ -278,9 +298,11 @@ class MainController:
                 jira_comment += "\n* " + repository['name'] + ":"
                 if base_pr:
                     jira_comment += "\n** " + base_version_branch + ": " + base_pr.html_url
+                else:
+                    jira_comment += " There are conflicts that needs to be manually treated."
 
                 if version_pr:
-                    jira_comment += "\n** " + sp_version_branch + ": " + version_pr.html_url
+                    jira_comment += "\n** " + sp_version_branch + ": " + base_pr.html_url
 
             # Add pull-request-sent label
             issue.fields.labels.append(u"pull-request-sent")
